3e9844d687617538f15a78c716a50ab226c7dbfb
Rasbandit <todd.rasband@gmail.com>
2016-11-14 10:08:58 -0700

    minified files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var concat = require("gulp-concat");
 var annotate = require("gulp-ng-annotate");
 var image = require('gulp-image');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename'); 
+var rename = require('gulp-rename');
+var minCss = require('gulp-minify-css');
+var livereload = require('gulp-livereload');
 
 var paths = {
    jsSource: ["./public/js/**/*.js"],
@@ -15,22 +17,27 @@ var paths = {
 };
 
 gulp.task('js', function() {
-   gulp.src(paths.jsSource).pipe(annotate()).pipe(concat("bundle.js")).pipe(uglify()).pipe(rename({extname: ".min.js"})).pipe(gulp.dest("./dist"));
+   gulp.src(paths.jsSource).pipe(annotate()).pipe(concat("bundle.js")).pipe(uglify()).pipe(rename({extname: ".min.js"})).pipe(gulp.dest("./dist")).pipe(livereload());
 });
 
 gulp.task('sass', function() {
-   gulp.src(paths.sassSource).pipe(sass()).pipe(concat("bundle.css")).pipe(gulp.dest("./dist"));
+   gulp.src(paths.sassSource)
+   .pipe(sass())
+   .pipe(concat("bundle.css"))
+   .pipe(gulp.dest("./dist"))
+   .pipe(livereload());
 });
 
 gulp.task('views', function() {
-   gulp.src(paths.viewsSource).pipe(gulp.dest("./dist/views"));
+   gulp.src(paths.viewsSource).pipe(gulp.dest("./dist/views")).pipe(livereload());
 })
 
 gulp.task('index', function() {
-   gulp.src(paths.indexSource).pipe(gulp.dest("./dist"));
+   gulp.src(paths.indexSource).pipe(gulp.dest("./dist")).pipe(livereload());
 });
 
 gulp.task('watch', function() {
+   livereload.listen();
    gulp.watch(paths.jsSource, ['js']);
    gulp.watch(paths.sassSource, ['sass']);
    gulp.watch(paths.indexSource, ['index']);
